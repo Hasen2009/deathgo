4c645cbd5b47a1e9200d702bf18800d1929bdb35
Hussen Shabeeb <hasen2009@yahoo.com>
2018-03-19 15:47:50 +0300

    first

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$('.header').height($(window).height());
+
+$('.conf-place .co-lg-8 .overlay p').css({
+  marginTop: $('.conf-place .co-lg-8 .overlay').height()/2 - $('.conf-place .co-lg-8 .overlay p').height()/2
+});
